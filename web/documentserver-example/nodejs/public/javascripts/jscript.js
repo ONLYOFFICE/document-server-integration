71fa334cc05f4e7f6cc2909fd5a857d547283512
Sergey Linnik <sergey.linnik@onlyoffice.com>
2016-05-18 17:57:46 +0300

    fix embedded mode

--- web/documentserver-example/nodejs/public/javascripts/jscript.js
+++ web/documentserver-example/nodejs/public/javascripts/jscript.js
@@ -236,7 +236,7 @@ if (typeof jQuery != "undefined") {
 
     jq(document).on("click", "#beginEmbedded:not(.disable)", function () {
         var fileId = encodeURIComponent(jq('#hiddenFileName').val());
-        var url = UrlEditor + "?mode=embedded&fileName=" + fileId + "&lang=" + language + "&userid=" + userid + "&firstname=" +firstname +"&lastname=" + lastname;
+        var url = UrlEditor + "?type=embedded&fileName=" + fileId + "&lang=" + language + "&userid=" + userid + "&firstname=" +firstname +"&lastname=" + lastname;
 
         jq("#mainProgress").addClass("embedded");
         jq("#beginEmbedded").addClass("disable");
