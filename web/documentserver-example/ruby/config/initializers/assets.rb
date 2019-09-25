057d38d0634cdc21423f76e76137a2c67129b8ef
Andrey Panov <gensurkin@yandex.ru>
2019-09-25 14:08:25 +0300

    ruby: body overflow style issue

--- web/documentserver-example/ruby/config/initializers/assets.rb
+++ web/documentserver-example/ruby/config/initializers/assets.rb
@@ -2,6 +2,9 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( editor.css )
+Rails.application.config.assets.precompile += %w( jquery-ui.css )
+Rails.application.config.assets.precompile += %w( stylesheet.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
