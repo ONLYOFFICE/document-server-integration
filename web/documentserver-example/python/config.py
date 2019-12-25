a1c524df00de594ffe2558fd40cded130cc22a16
Andrey Panov <gensurkin@yandex.ru>
2019-12-25 11:28:12 +0300

    python: changed docserv default link

--- web/documentserver-example/python/config.py
+++ web/documentserver-example/python/config.py
@@ -14,8 +14,8 @@ DOC_SERV_CONVERT = [
 DOC_SERV_TIMEOUT = 120000
 
 DOC_SERV_CONVERTER_URL = 'https://documentserver/ConvertService.ashx'
-DOC_SERV_API_URL = 'https://documentserver/web-apps-pro/apps/api/documents/api.js'
-DOC_SERV_PRELOADER_URL = 'https://documentserver/web-apps-pro/apps/api/documents/cache-scripts.html'
+DOC_SERV_API_URL = 'https://documentserver/web-apps/apps/api/documents/api.js'
+DOC_SERV_PRELOADER_URL = 'https://documentserver/web-apps/apps/api/documents/cache-scripts.html'
 
 EXAMPLE_DOMAIN = 'https://exampleserver/'
 
