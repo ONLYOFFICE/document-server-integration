c3c168917001af0745d90ec990f70df126c742f8
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2020-01-09 14:07:00 +0300

    nodejs: fix upload files
    (cherry picked from commit af7101844acb43ff2cf844d80ec834dca405ccda)

--- web/documentserver-example/nodejs/run-develop.py
+++ web/documentserver-example/nodejs/run-develop.py
@@ -8,14 +8,11 @@ def install_module():
   base.print_info('Install')
   base.cmd('npm', ['install'])
 
-def run_module(directory, args=[]):
-  base.run_nodejs_in_dir(directory, args)
-
 def run_integration_example():
   install_module()
-  base.set_env('NODE_CONFIG_DIR', '../config')
+  base.set_env('NODE_CONFIG_DIR', './config')
   base.print_info('run integration example')
-  run_module('bin', ['www'])
+  base.run_nodejs(['bin/www'])
 
 base.set_env('NODE_ENV', 'development-windows')
 
