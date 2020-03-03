67d6eaa18bb7b60bdbc88338cfaf9931a8811fc4
Andrey Panov <gensurkin@yandex.ru>
2020-03-03 19:03:05 +0300

    python: env vars

--- web/documentserver-example/python/config.py
+++ web/documentserver-example/python/config.py
@@ -1,3 +1,5 @@
+import os
+
 FILE_SIZE_MAX = 5242880
 STORAGE_PATH = 'app_data'
 
@@ -42,3 +44,12 @@ EXT_DOCUMENT = [
     ".html", ".htm", ".mht",
     ".pdf", ".djvu", ".fb2", ".epub", ".xps"
 ]
+
+
+if os.environ.get("EXAMPLE_DOMAIN"):
+    EXAMPLE_DOMAIN = os.environ.get("EXAMPLE_DOMAIN")
+if os.environ.get("DOC_SERV"):
+    base = os.environ.get("DOC_SERV").rstrip('/')
+    DOC_SERV_CONVERTER_URL = base + '/ConvertService.ashx'
+    DOC_SERV_API_URL = base + '/web-apps/apps/api/documents/api.js'
+    DOC_SERV_PRELOADER_URL = base + '/web-apps/apps/api/documents/cache-scripts.html'
