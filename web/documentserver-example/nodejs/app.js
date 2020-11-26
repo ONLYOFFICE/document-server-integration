0f2695fba07521c752452e12143dca9986c5a336
Alexandr Fedorov <aldrfedorov97@mail.ru>
2020-11-26 18:16:44 +0300

    nodejs: jwt for compare-file

--- web/documentserver-example/nodejs/app.js
+++ web/documentserver-example/nodejs/app.js
@@ -666,6 +666,7 @@ app.get("/editor", function (req, res) {
                     console.log(err);
                 } else {
                     argss.editor.token = jwt.sign(JSON.parse("{"+html+"}"), cfgSignatureSecret, {expiresIn: cfgSignatureSecretExpiresIn});
+                    argss.dataCompareFile.token = jwt.sign(argss.dataCompareFile, cfgSignatureSecret, {expiresIn: cfgSignatureSecretExpiresIn});
                 }
                 res.render("editor", argss);
               });
