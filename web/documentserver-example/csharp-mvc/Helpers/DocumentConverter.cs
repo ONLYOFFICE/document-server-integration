b2ee170ecb9d6ffdfbdfdf54c8227d7e72a58b58
Alexandr Fedorov <aldrfedorov97@mail.ru>
2021-01-22 14:43:51 +0300

    csharp: add files.docservice.header

--- web/documentserver-example/csharp-mvc/Helpers/DocumentConverter.cs
+++ web/documentserver-example/csharp-mvc/Helpers/DocumentConverter.cs
@@ -119,7 +119,8 @@ namespace OnlineEditorsExampleMVC.Helpers
 
                 var payloadToken = JwtManager.Encode(payload);
                 var bodyToken = JwtManager.Encode(body);
-                request.Headers.Add("Authorization", "Bearer " + payloadToken);
+                string JWTheader = WebConfigurationManager.AppSettings["files.docservice.header"].Equals("") ? "Authorization" : WebConfigurationManager.AppSettings["files.docservice.url.header"];
+                request.Headers.Add(JWTheader, "Bearer " + payloadToken);
 
                 body.Add("token", bodyToken);
             }
