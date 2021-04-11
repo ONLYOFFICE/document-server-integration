f788379eb58ffde38e9d704cfa88eb9f97d21bba
Alexandr Fedorov <aldrfedorov97@mail.ru>
2021-04-11 19:44:37 +0300

    csharp-mvc: permissions.edit add condition view

--- web/documentserver-example/csharp-mvc/Models/FileModel.cs
+++ web/documentserver-example/csharp-mvc/Models/FileModel.cs
@@ -118,7 +118,7 @@ namespace OnlineEditorsExampleMVC.Models
                                         {
                                             { "comment", editorsMode != "view" && editorsMode != "fillForms" && editorsMode != "embedded" && editorsMode != "blockcontent" },
                                             { "download", true },
-                                            { "edit", canEdit && (editorsMode == "edit" || editorsMode == "filter") || editorsMode == "blockcontent" },
+                                            { "edit", canEdit && (editorsMode == "edit" || editorsMode == "view" || editorsMode == "filter" || editorsMode == "blockcontent") },
                                             { "fillForms", editorsMode != "view" && editorsMode != "comment" && editorsMode != "embedded" && editorsMode != "blockcontent" },
                                             { "modifyFilter", editorsMode != "filter" },
                                             { "modifyContentControl", editorsMode != "blockcontent" },
