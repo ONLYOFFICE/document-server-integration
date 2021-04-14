beaffd40de808b271c4ac2cfbb5bc5eeac8ce314
Natalia <ovchinnikova.natalya@onlyoffice.com>
2021-04-14 14:14:01 +0300

    java: added comments

--- web/documentserver-example/java/src/main/java/helpers/ServiceConverter.java
+++ web/documentserver-example/java/src/main/java/helpers/ServiceConverter.java
@@ -59,10 +59,11 @@ public class ServiceConverter
     {
         try
         {
+            // get timeout value from the settings.properties
             int timeout = Integer.parseInt(ConfigManager.GetProperty("files.docservice.timeout"));
-            if (timeout > 0) 
+            if (timeout > 0)  // if it's greater than 0
             {
-                ConvertTimeout = timeout;
+                ConvertTimeout = timeout;  // assign this value to a convert timeout
             }
         }
         catch (Exception ex)
@@ -70,17 +71,21 @@ public class ServiceConverter
         }
     }
 
+    // get the url of the converted file
     public static String GetConvertedUri(String documentUri, String fromExtension, String toExtension, String documentRevisionId, String filePass, Boolean isAsync) throws Exception
     {
+        // check if the fromExtension parameter is defined; if not, get it from the document url
         fromExtension = fromExtension == null || fromExtension.isEmpty() ? FileUtility.GetFileExtension(documentUri) : fromExtension;
 
+        // check if the file name parameter is defined; if not, get random uuid for this file
         String title = FileUtility.GetFileName(documentUri);
         title = title == null || title.isEmpty() ? UUID.randomUUID().toString() : title;
 
         documentRevisionId = documentRevisionId == null || documentRevisionId.isEmpty() ? documentUri : documentRevisionId;
 
-        documentRevisionId = GenerateRevisionId(documentRevisionId);
+        documentRevisionId = GenerateRevisionId(documentRevisionId);  // create document token
 
+        // write all the necessary parameters to the body object
         ConvertBody body = new ConvertBody();
         body.url = documentUri;
         body.outputtype = toExtension.replace(".", "");
@@ -104,12 +109,13 @@ public class ServiceConverter
             if (isAsync)
                 map.put("async", body.async);
 
+            // add token to the body if it is enabled
             String token = DocumentManager.CreateToken(map);
             body.token = token;
 
             Map<String, Object> payloadMap = new HashMap<String, Object>();
-            payloadMap.put("payload", map);
-            headerToken = DocumentManager.CreateToken(payloadMap);
+            payloadMap.put("payload", map);  // create payload object
+            headerToken = DocumentManager.CreateToken(payloadMap);  // create header token
         }
 
         Gson gson = new Gson();
@@ -117,6 +123,7 @@ public class ServiceConverter
 
         byte[] bodyByte = bodyString.getBytes(StandardCharsets.UTF_8);
 
+        // specify request parameters
         URL url = new URL(DocumentConverterUrl);
         java.net.HttpURLConnection connection = (java.net.HttpURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
@@ -126,6 +133,7 @@ public class ServiceConverter
         connection.setRequestProperty("Accept", "application/json");
         connection.setConnectTimeout(ConvertTimeout);
 
+        // write header token to the request
         if (DocumentManager.TokenEnabled())
         {
             connection.setRequestProperty(DocumentJwtHeader.equals("") ? "Authorization" : DocumentJwtHeader, "Bearer " + headerToken);
@@ -141,6 +149,7 @@ public class ServiceConverter
         if (stream == null)
             throw new Exception("Could not get an answer");
 
+        // convert string to json
         String jsonString = ConvertStreamToString(stream);
 
         connection.disconnect();
@@ -148,21 +157,24 @@ public class ServiceConverter
         return GetResponseUri(jsonString);
     }
 
+    // generate document key
     public static String GenerateRevisionId(String expectedKey)
     {
-        if (expectedKey.length() > 20)
-            expectedKey = Integer.toString(expectedKey.hashCode());
+        if (expectedKey.length() > 20)  // if the expected key length is greater than 20
+            expectedKey = Integer.toString(expectedKey.hashCode());  // the expected key is hashed and a fixed length value is stored in the string format
 
         String key = expectedKey.replace("[^0-9-.a-zA-Z_=]", "_");
 
-        return key.substring(0, Math.min(key.length(), 20));
+        return key.substring(0, Math.min(key.length(), 20));  // the resulting key length is 20 or less
     }
 
+    // create an error message for an error code
     private static void ProcessConvertServiceResponceError(int errorCode) throws Exception
     {
         String errorMessage = "";
         String errorMessageTemplate = "Error occurred in the ConvertService: ";
 
+        // add the error message to the error message template depending on the error code
         switch (errorCode)
         {
             case -8:
@@ -189,53 +201,56 @@ public class ServiceConverter
             case -1:
                 errorMessage = errorMessageTemplate + "Error convertation unknown";
                 break;
-            case 0:
+            case 0:  // if the error code is equal to 0, the error message is empty
                 break;
             default:
-                errorMessage = "ErrorCode = " + errorCode;
+                errorMessage = "ErrorCode = " + errorCode;  // default value for the error message
                 break;
         }
 
         throw new Exception(errorMessage);
     }
 
+    // get the response url
     private static String GetResponseUri(String jsonString) throws Exception
     {
         JSONObject jsonObj = ConvertStringToJSON(jsonString);
 
         Object error = jsonObj.get("error");
-        if (error != null)
-            ProcessConvertServiceResponceError(Math.toIntExact((long)error));
+        if (error != null)  // if an error occurs
+            ProcessConvertServiceResponceError(Math.toIntExact((long)error));  // then get an error message
 
+        // check if the conversion is completed and save the result to a variable
         Boolean isEndConvert = (Boolean) jsonObj.get("endConvert");
 
         Long resultPercent = 0l;
         String responseUri = null;
 
-        if (isEndConvert)
+        if (isEndConvert)  // if the conversion is completed
         {
             resultPercent = 100l;
-            responseUri = (String) jsonObj.get("fileUrl");
+            responseUri = (String) jsonObj.get("fileUrl");  // get the file url
         }
-        else
+        else  // if the conversion isn't completed
         {
             resultPercent = (Long) jsonObj.get("percent");
-            resultPercent = resultPercent >= 100l ? 99l : resultPercent;
+            resultPercent = resultPercent >= 100l ? 99l : resultPercent;  // get the percentage value
         }
 
         return resultPercent >= 100l ? responseUri : "";
     }
 
+    // convert stream to string
     public static String ConvertStreamToString(InputStream stream) throws IOException
     {
-        InputStreamReader inputStreamReader = new InputStreamReader(stream);
-        StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        String line = bufferedReader.readLine();
+        InputStreamReader inputStreamReader = new InputStreamReader(stream);  // create an object to get incoming stream
+        StringBuilder stringBuilder = new StringBuilder();  // create a string builder object
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);  // create an object to read incoming streams
+        String line = bufferedReader.readLine();  // get incoming streams by lines
 
         while (line != null)
         {
-            stringBuilder.append(line);
+            stringBuilder.append(line);  // concatenate strings using the string builder
             line = bufferedReader.readLine();
         }
 
@@ -244,11 +259,12 @@ public class ServiceConverter
         return result;
     }
 
+    // convert string to json
     public static JSONObject ConvertStringToJSON(String jsonString) throws ParseException
     {
         JSONParser parser = new JSONParser();
-        Object obj = parser.parse(jsonString);
-        JSONObject jsonObj = (JSONObject) obj;
+        Object obj = parser.parse(jsonString);  // parse json string
+        JSONObject jsonObj = (JSONObject) obj;  // and turn it into a json object
 
         return jsonObj;
     }
--- web/documentserver-example/java/src/main/java/helpers/TrackManager.java
+++ web/documentserver-example/java/src/main/java/helpers/TrackManager.java
@@ -36,10 +36,12 @@ import java.util.Scanner;
 public class TrackManager {
     private static final String DocumentJwtHeader = ConfigManager.GetProperty("files.docservice.header");
 
+    // read request body
     public static JSONObject readBody(HttpServletRequest request, PrintWriter writer) throws Exception {
         String bodyString = "";
 
         try {
+            // read request body by streams
             Scanner scanner = new Scanner(request.getInputStream());
             scanner.useDelimiter("\\A");
             bodyString = scanner.hasNext() ? scanner.next() : "";
@@ -50,6 +52,7 @@ public class TrackManager {
             throw ex;
         }
 
+        // error when the bodyString object is empty
         if (bodyString.isEmpty()) {
             writer.write("empty request.getInputStream");
             throw new Exception("empty request.getInputStream");
@@ -59,35 +62,36 @@ public class TrackManager {
         JSONObject body;
 
         try {
-            Object obj = parser.parse(bodyString);
+            Object obj = parser.parse(bodyString);  // parse bodyString object
             body = (JSONObject) obj;
         } catch (Exception ex) {
             writer.write("JSONParser.parse error:" + ex.getMessage());
             throw ex;
         }
 
+        // if the secret key to generate token exists
         if (DocumentManager.TokenEnabled()) {
-            String token = (String) body.get("token");
+            String token = (String) body.get("token");  // get the document token
 
-            if (token == null) {
-                String header = (String) request.getHeader(DocumentJwtHeader == null || DocumentJwtHeader.isEmpty() ? "Authorization" : DocumentJwtHeader);
+            if (token == null) {  // if JSON web token is not received
+                String header = (String) request.getHeader(DocumentJwtHeader == null || DocumentJwtHeader.isEmpty() ? "Authorization" : DocumentJwtHeader);  // get it from the Authorization header
                 if (header != null && !header.isEmpty()) {
-                    token = header.startsWith("Bearer ") ? header.substring(7) : header;
+                    token = header.startsWith("Bearer ") ? header.substring(7) : header;  // and save it without Authorization prefix
                 }
             }
 
-            if (token == null || token.isEmpty()) {
-                writer.write("{\"error\":1,\"message\":\"JWT expected\"}");
+            if (token == null || token.isEmpty()) {  // if the token is not received
+                writer.write("{\"error\":1,\"message\":\"JWT expected\"}");  // an error occurs
                 throw new Exception("{\"error\":1,\"message\":\"JWT expected\"}");
             }
 
-            JWT jwt = DocumentManager.ReadToken(token);
+            JWT jwt = DocumentManager.ReadToken(token);  // read token
             if (jwt == null) {
-                writer.write("{\"error\":1,\"message\":\"JWT validation failed\"}");
+                writer.write("{\"error\":1,\"message\":\"JWT validation failed\"}");  // an error occurs
                 throw new Exception("{\"error\":1,\"message\":\"JWT validation failed\"}");
             }
 
-            if (jwt.getObject("payload") != null) {
+            if (jwt.getObject("payload") != null) {  // get the payload object from the request body
                 try {
                     @SuppressWarnings("unchecked") LinkedHashMap<String, Object> payload =
                             (LinkedHashMap<String, Object>)jwt.getObject("payload");
@@ -112,20 +116,22 @@ public class TrackManager {
         return body;
     }
 
+    // file saving process
     public static void processSave(JSONObject body, String fileName, String userAddress) throws Exception {
         String downloadUri = (String) body.get("url");
         String changesUri = (String) body.get("changesurl");
         String key = (String) body.get("key");
         String newFileName = fileName;
 
-        String curExt = FileUtility.GetFileExtension(fileName);
-        String downloadExt = FileUtility.GetFileExtension(downloadUri);
+        String curExt = FileUtility.GetFileExtension(fileName);  // get current file extension
+        String downloadExt = FileUtility.GetFileExtension(downloadUri);  // get the extension of the downloaded file
 
+        // convert downloaded file to the file with the current extension if these extensions aren't equal
         if (!curExt.equals(downloadExt)) {
             try {
-                String newFileUri = ServiceConverter.GetConvertedUri(downloadUri, downloadExt, curExt, ServiceConverter.GenerateRevisionId(downloadUri), null, false);
+                String newFileUri = ServiceConverter.GetConvertedUri(downloadUri, downloadExt, curExt, ServiceConverter.GenerateRevisionId(downloadUri), null, false);  // convert file and get url to a new file
                 if (newFileUri.isEmpty()) {
-                    newFileName = DocumentManager.GetCorrectName(FileUtility.GetFileNameWithoutExtension(fileName) + downloadExt, userAddress);
+                    newFileName = DocumentManager.GetCorrectName(FileUtility.GetFileNameWithoutExtension(fileName) + downloadExt, userAddress);  // get the correct file name if it already exists
                 } else {
                     downloadUri = newFileUri;
                 }
@@ -134,54 +140,56 @@ public class TrackManager {
             }
         }
 
-        String storagePath = DocumentManager.StoragePath(newFileName, userAddress);
-        File histDir = new File(DocumentManager.HistoryDir(storagePath));
-        if (!histDir.exists()) histDir.mkdirs();
+        String storagePath = DocumentManager.StoragePath(newFileName, userAddress);  // get the file path
+        File histDir = new File(DocumentManager.HistoryDir(storagePath));  // get the path to the history direction
+        if (!histDir.exists()) histDir.mkdirs();  // if the path doesn't exist, create it
 
-        String versionDir = DocumentManager.VersionDir(histDir.getAbsolutePath(), DocumentManager.GetFileVersion(histDir.getAbsolutePath()));
+        String versionDir = DocumentManager.VersionDir(histDir.getAbsolutePath(), DocumentManager.GetFileVersion(histDir.getAbsolutePath()));  // get the path to the file version
         File ver = new File(versionDir);
         File lastVersion = new File(DocumentManager.StoragePath(fileName, userAddress));
         File toSave = new File(storagePath);
 
         if (!ver.exists()) ver.mkdirs();
 
-        lastVersion.renameTo(new File(versionDir + File.separator + "prev" + curExt));
+        lastVersion.renameTo(new File(versionDir + File.separator + "prev" + curExt));  // get the path to the previous file version and rename the last file version with it
 
-        downloadToFile(downloadUri, toSave);
-        downloadToFile(changesUri, new File(versionDir + File.separator + "diff.zip"));
+        downloadToFile(downloadUri, toSave);  // save file to the storage path 
+        downloadToFile(changesUri, new File(versionDir + File.separator + "diff.zip"));  // save file changes to the diff.zip archive
 
         String history = (String) body.get("changeshistory");
         if (history == null && body.containsKey("history")) {
             history = ((JSONObject) body.get("history")).toJSONString();
         }
         if (history != null && !history.isEmpty()) {
-            FileWriter fw = new FileWriter(new File(versionDir + File.separator + "changes.json"));
+            FileWriter fw = new FileWriter(new File(versionDir + File.separator + "changes.json"));  // write the history changes to the changes.json file
             fw.write(history);
             fw.close();
         }
 
-        FileWriter fw = new FileWriter(new File(versionDir + File.separator + "key.txt"));
+        FileWriter fw = new FileWriter(new File(versionDir + File.separator + "key.txt"));  // write the key value to the key.txt file
         fw.write(key);
         fw.close();
 
-        String forcesavePath = DocumentManager.ForcesavePath(newFileName, userAddress, false);
-        if (!forcesavePath.equals("")) {
+        String forcesavePath = DocumentManager.ForcesavePath(newFileName, userAddress, false);  // get the path to the forcesaved file version
+        if (!forcesavePath.equals("")) {  // if the forcesaved file version exists
             File forceSaveFile = new File(forcesavePath);
-            forceSaveFile.delete();
+            forceSaveFile.delete();  // remove it
         }
     }
 
+    // file force saving process
     public static void processForceSave(JSONObject body, String fileName, String userAddress) throws Exception {
 
         String downloadUri = (String) body.get("url");
 
-        String curExt = FileUtility.GetFileExtension(fileName);
-        String downloadExt = FileUtility.GetFileExtension(downloadUri);
+        String curExt = FileUtility.GetFileExtension(fileName);  // get current file extension
+        String downloadExt = FileUtility.GetFileExtension(downloadUri);  // get the extension of the downloaded file
         Boolean newFileName = false;
 
+        // convert downloaded file to the file with the current extension if these extensions aren't equal
         if (!curExt.equals(downloadExt)) {
             try {
-                String newFileUri = ServiceConverter.GetConvertedUri(downloadUri, downloadExt, curExt, ServiceConverter.GenerateRevisionId(downloadUri), null, false);
+                String newFileUri = ServiceConverter.GetConvertedUri(downloadUri, downloadExt, curExt, ServiceConverter.GenerateRevisionId(downloadUri), null, false);  // convert file and get url to a new file
                 if (newFileUri.isEmpty()) {
                     newFileName = true;
                 } else {
@@ -193,12 +201,12 @@ public class TrackManager {
         }
 
         String forcesavePath = "";
-        boolean isSubmitForm = body.get("forcesavetype").toString().equals("3");
+        boolean isSubmitForm = body.get("forcesavetype").toString().equals("3");  // SubmitForm
 
-        if (isSubmitForm) {
-            //new file
+        if (isSubmitForm) {  // if the form is submitted
+            // new file
             if (newFileName){
-                fileName = DocumentManager.GetCorrectName(FileUtility.GetFileNameWithoutExtension(fileName) + "-form" + downloadExt, userAddress);
+                fileName = DocumentManager.GetCorrectName(FileUtility.GetFileNameWithoutExtension(fileName) + "-form" + downloadExt, userAddress);  // get the correct file name if it already exists
             } else {
                 fileName = DocumentManager.GetCorrectName(FileUtility.GetFileNameWithoutExtension(fileName) + "-form" + curExt, userAddress);
             }
@@ -208,7 +216,8 @@ public class TrackManager {
                 fileName = DocumentManager.GetCorrectName(FileUtility.GetFileNameWithoutExtension(fileName) + downloadExt, userAddress);
             }
 
-            forcesavePath = DocumentManager.ForcesavePath(fileName, userAddress, false);
+            // create forcesave path if it doesn't exist
+            forcesavePath = DocumentManager.ForcesavePath(newFileName, userAddress, false);
             if (forcesavePath == "") {
                 forcesavePath = DocumentManager.ForcesavePath(fileName, userAddress, true);
             }
@@ -220,18 +229,19 @@ public class TrackManager {
         if (isSubmitForm) {
             JSONArray actions = (JSONArray) body.get("actions");
             JSONObject action = (JSONObject) actions.get(0);
-            String user = (String) action.get("userid");
-            DocumentManager.CreateMeta(fileName, user, "Filling Form", userAddress);
+            String user = (String) action.get("userid");  // get the user id
+            DocumentManager.CreateMeta(fileName, user, "Filling Form", userAddress);  // create meta data for forcesaved file
         }
     }
 
+    // save file information from the url to the file specified
     private static void downloadToFile(String url, File file) throws Exception {
-        if (url == null || url.isEmpty()) throw new Exception("argument url");
-        if (file == null) throw new Exception("argument path");
+        if (url == null || url.isEmpty()) throw new Exception("argument url");  // url isn't specified
+        if (file == null) throw new Exception("argument path");  // file isn't specified
 
         URL uri = new URL(url);
         java.net.HttpURLConnection connection = (java.net.HttpURLConnection) uri.openConnection();
-        InputStream stream = connection.getInputStream();
+        InputStream stream = connection.getInputStream();  // get input stream of the file information from the url
 
         if (stream == null)
         {
@@ -244,15 +254,17 @@ public class TrackManager {
             final byte[] bytes = new byte[1024];
             while ((read = stream.read(bytes)) != -1)
             {
-                out.write(bytes, 0, read);
+                out.write(bytes, 0, read);  // write bytes to the output stream
             }
 
+            // force write data to the output stream that can be cached in the current thread
             out.flush();
         }
 
         connection.disconnect();
     }
 
+    // create a command request
     public static void commandRequest(String method, String key) throws Exception {
         String DocumentCommandUrl = ConfigManager.GetProperty("files.docservice.url.site") + ConfigManager.GetProperty("files.docservice.url.command");
 
@@ -264,15 +276,16 @@ public class TrackManager {
         params.put("key", key);
 
         String headerToken = "";
-        if (DocumentManager.TokenEnabled())
+        if (DocumentManager.TokenEnabled())  // check if a secret key to generate token exists or not
         {
             Map<String, Object> payloadMap = new HashMap<String, Object>();
             payloadMap.put("payload", params);
-            headerToken = DocumentManager.CreateToken(payloadMap);
+            headerToken = DocumentManager.CreateToken(payloadMap);  // encode a payload object into a header token
 
+            // add a header Authorization with a header token and Authorization prefix in it
             connection.setRequestProperty(DocumentJwtHeader.equals("") ? "Authorization" : DocumentJwtHeader, "Bearer " + headerToken);
 
-            String token = DocumentManager.CreateToken(params);
+            String token = DocumentManager.CreateToken(params);  // encode a payload object into a body token
             params.put("token", token);
         }
 
@@ -281,23 +294,23 @@ public class TrackManager {
 
         byte[] bodyByte = bodyString.getBytes(StandardCharsets.UTF_8);
 
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");  // set the request method
+        connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");  // set the Content-Type header
+        connection.setDoOutput(true); // set the doOutput field to true
 
         connection.connect();
         try (OutputStream os = connection.getOutputStream()) {
-            os.write(bodyByte);
+            os.write(bodyByte);  // write bytes to the output stream
         }
-        InputStream stream = connection.getInputStream();;
+        InputStream stream = connection.getInputStream();;  // get input stream
 
         if (stream == null)
             throw new Exception("Could not get an answer");
 
-        String jsonString = ServiceConverter.ConvertStreamToString(stream);
+        String jsonString = ServiceConverter.ConvertStreamToString(stream);  // convert stream to json string
         connection.disconnect();
 
-        JSONObject response = ServiceConverter.ConvertStringToJSON(jsonString);
+        JSONObject response = ServiceConverter.ConvertStringToJSON(jsonString);  // convert json string to json object
         if (!response.get("error").toString().equals("0")){
             throw new Exception(response.toJSONString());
         }
