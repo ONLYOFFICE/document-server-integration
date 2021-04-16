d8d068d2607d66055cada7b1fc56dbeeb10968bc
Alexandr Fedorov <aldrfedorov97@mail.ru>
2021-04-16 18:31:20 +0300

    php: reload files list after closing upload dialog by Close button

--- web/documentserver-example/php/js/jscript.js
+++ web/documentserver-example/php/js/jscript.js
@@ -19,6 +19,8 @@
 if (typeof jQuery != "undefined") {
     jq = jQuery.noConflict();
 
+    mustReload = false;
+
     user = getUrlVars()["user"];
     if ("" != user && undefined != user)
         jq("#user").val(user);
@@ -69,6 +71,8 @@ if (typeof jQuery != "undefined") {
                 jq("#hiddenFileName").val(response.filename);
                 jq("#uploadFileName").text(response.filename);
                 jq("#uploadFileName").addClass(response.documentType);
+
+                mustReload = true;
 
                 jq("#step1").addClass("done").removeClass("current");
 
@@ -271,6 +275,9 @@ if (typeof jQuery != "undefined") {
         jq('#hiddenFileName').val("");
         jq("#embeddedView").attr("src", "");
         jq.unblockUI();
+        if (mustReload) {
+            document.location.reload();
+        }
     });
 
     jq(document).on("click", ".delete-file", function () {
