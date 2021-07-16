08072226a5adb175859a4bdc25a982807b5bb3e7
metoou <shamil.shaihutdinov@onlyoffice.com>
2021-07-16 19:31:28 +0300

    csharp-core: added new class for serialize responseFromService

---
+++ web/documentserver-example/csharp-core/OnlineEditorsExampleNetCore/Services/ResponseFromConvert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace OnlineEditorsExampleNetCore.Services
+{
+    public class ResponseFromConvert
+    {
+        [JsonPropertyName("error")]
+        public string Error { get; set; }
+
+        [JsonPropertyName("endConvert")]
+        public bool EndConvert { get; set; }
+
+        [JsonPropertyName("fileUrl")]
+        public string FileUrl { get; set; }
+
+        [JsonPropertyName("percent")]
+        public int Percent { get; set; }
+    }
+}
