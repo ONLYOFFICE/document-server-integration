ce18f76698b3922e00a52ec73834eb1f0e03b818
Oleg Sinizin <oleg.sinizin@onlyoffice.com>
2021-09-23 21:55:38 +0300

    cors, fix regexp, fix manifest

--- web/documentserver-example/ruby/config/application.rb
+++ web/documentserver-example/ruby/config/application.rb
@@ -16,6 +16,16 @@ Bundler.require(*Rails.groups)
 
 module OnlineEditorsExampleRuby
   class Application < Rails::Application
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+                 headers: :any,
+                 methods: [:get, :post, :patch, :delete, :put, :options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
