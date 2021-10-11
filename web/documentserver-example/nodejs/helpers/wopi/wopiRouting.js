d4f4b2bd9578741dd0f8c79ab13fa112e45800b9
Oleg Sinizin <oleg.sinizin@onlyoffice.com>
2021-10-11 14:09:16 +0300

    nodejs: change address builder

--- web/documentserver-example/nodejs/helpers/wopi/wopiRouting.js
+++ web/documentserver-example/nodejs/helpers/wopi/wopiRouting.js
@@ -36,13 +36,7 @@ exports.registerRoutes = function(app) {
 
         let absSiteUrl = siteUrl;
         if (absSiteUrl.indexOf("/") === 0) {
-            let host = req.get("host");
-            let pos = host.indexOf("/", "https://".length);
-            if (pos > -1)
-            {
-                host = host.substring(0, pos);
-            }
-            absSiteUrl = docManager.getProtocol() + "://" + host + siteUrl;
+            absSiteUrl = docManager.getServerHost() + siteUrl;
         }
 
         // get the wopi discovery information
