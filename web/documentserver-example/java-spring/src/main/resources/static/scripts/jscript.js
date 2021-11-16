c8aac65af621fe9c33274973d743ef505966c1fc
Oleg Sinizin <oleg.sinizin@onlyoffice.com>
2021-11-16 12:56:28 +0300

    java-spring: opening uploaded form

--- web/documentserver-example/java-spring/src/main/resources/static/scripts/jscript.js
+++ web/documentserver-example/java-spring/src/main/resources/static/scripts/jscript.js
@@ -167,7 +167,7 @@ if (typeof jQuery !== "undefined") {
         var posExt = fileName.lastIndexOf(".");
         posExt = 0 <= posExt ? fileName.substring(posExt).trim().toLowerCase() : "";
 
-        if (EditedExtList.indexOf(posExt) !== -1) {
+        if (EditedExtList.indexOf(posExt) !== -1 || FillExtList.indexOf(posExt) !== -1) {
             jq("#beginEdit").removeClass("disable");
         }
     };
