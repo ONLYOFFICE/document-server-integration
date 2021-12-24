13b3eed5ed135a6057993d327e1953a05238ea87
Andrey Yumatov <Andrey.yumatov@onlyoffice.com>
2021-12-24 18:12:40 +0300

    fileType instead parsing url to save doc in C# (take fileType out of body)

--- web/documentserver-example/csharp/TrackManager.cs
+++ web/documentserver-example/csharp/TrackManager.cs
@@ -97,7 +97,7 @@ namespace OnlineEditorsExample
             }
             var downloadUri = (string)fileData["url"];
             var curExt = Path.GetExtension(fileName).ToLower();  // get current file extension
-            var downloadExt = Path.GetExtension(downloadUri).ToLower() ?? "";  // get the extension of the downloaded file
+            var downloadExt = "."+(string)fileData["filetype"]; // get the extension of the downloaded file
             var newFileName = fileName;
 
             // convert downloaded file to the file with the current extension if these extensions aren't equal
@@ -175,7 +175,7 @@ namespace OnlineEditorsExample
             var downloadUri = (string)fileData["url"];
 
             string curExt = Path.GetExtension(fileName).ToLower();  // get current file extension
-            string downloadExt = Path.GetExtension(downloadUri).ToLower();  // get the extension of the downloaded file
+            string downloadExt = "."+(string)fileData["filetype"];  // get the extension of the downloaded file
             Boolean newFileName = false;
 
             // convert downloaded file to the file with the current extension if these extensions aren't equal
