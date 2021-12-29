783ad9f2deec66181a1a54b195240651a387e9a5
Andrey Malygin <Andrey.Malygin@onlyoffice.com>
2021-12-29 13:30:11 +0300

    fix Customization in Java and JavaSpringExample

--- web/documentserver-example/java-spring/src/main/java/com/onlyoffice/integration/documentserver/models/configurations/Goback.java
+++ web/documentserver-example/java-spring/src/main/java/com/onlyoffice/integration/documentserver/models/configurations/Goback.java
@@ -1,26 +1,23 @@
 /**
- *
  * (c) Copyright Ascensio System SIA 2021
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.onlyoffice.integration.documentserver.models.configurations;
 
 import com.onlyoffice.integration.documentserver.storage.FileStoragePathBuilder;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
@@ -30,8 +27,6 @@ import javax.annotation.PostConstruct;
 
 @Component
 @Scope("prototype")
-@Getter
-@Setter
 public class Goback {  // the settings for the Open file location menu button and upper right corner button
 
     @Autowired
@@ -39,10 +34,12 @@ public class Goback {  // the settings for the Open file location menu button an
 
     @Value("${url.index}")
     private String indexMapping;
+
+    @Getter
     private String url;  // the absolute URL to the website address which will be opened when clicking the Open file location menu button
 
     @PostConstruct
-    private void init(){
-        this.url = storagePathBuilder.getServerUrl(false)+indexMapping;
+    private void init() {
+        this.url = storagePathBuilder.getServerUrl(false) + indexMapping;
     }
 }
