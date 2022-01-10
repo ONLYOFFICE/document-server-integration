05706ff038c3e0ac98fe14b7ccf339d6d699198c
Andrey Yumatov <Andrey.yumatov@onlyoffice.com>
2022-01-10 18:45:27 +0300

    PHP: fix[support old version] fileType instead parsing

--- web/documentserver-example/php/trackmanager.php
+++ web/documentserver-example/php/trackmanager.php
@@ -83,6 +83,10 @@ function processSave($data, $fileName, $userAddress) {
 
     $curExt = strtolower('.' . pathinfo($fileName, PATHINFO_EXTENSION));  // get current file extension
     $downloadExt = strtolower('.' . $data["filetype"]);  // get the extension of the downloaded file
+
+    // TODO [Delete in version 7.0 or higher]
+    if (!$downloadExt) $downloadExt = strtolower('.' . pathinfo($downloadUri, PATHINFO_EXTENSION));    // Support for versions below 7.0
+
     $newFileName = $fileName;
 
     // convert downloaded file to the file with the current extension if these extensions aren't equal
@@ -155,6 +159,10 @@ function processForceSave($data, $fileName, $userAddress) {
 
     $curExt = strtolower('.' . pathinfo($fileName, PATHINFO_EXTENSION));  // get current file extension
     $downloadExt = strtolower('.' . $data["filetype"]);  // get the extension of the downloaded file
+
+    // TODO [Delete in version 7.0 or higher]
+    if (!$downloadExt) $downloadExt = strtolower('.' . pathinfo($downloadUri, PATHINFO_EXTENSION));    // Support for versions below 7.0
+    
     $newFileName = false;
 
     // convert downloaded file to the file with the current extension if these extensions aren't equal
