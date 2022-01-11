064c0dcc35eb244ca300a5c36a5339e1b05e94f9
Andrey Yumatov <Andrey.yumatov@onlyoffice.com>
2022-01-11 16:42:26 +0300

    C#: fix code

--- web/documentserver-example/csharp/TrackManager.cs
+++ web/documentserver-example/csharp/TrackManager.cs
@@ -97,10 +97,14 @@ namespace OnlineEditorsExample
             }
             var downloadUri = (string)fileData["url"];
             var curExt = Path.GetExtension(fileName).ToLower();  // get current file extension
-            var downloadExt = "."+(string)fileData["filetype"]; // get the extension of the downloaded file
 
-            // TODO [Delete in version 7.0 or higher]
-            if (downloadExt == null) {
+            try
+            {
+                var downloadExt = "."+(string)fileData["filetype"]; // get the extension of the downloaded file
+            }
+            catch (System.Exception)
+            {   
+                // TODO [Delete in version 7.0 or higher]
                 downloadExt = Path.GetExtension(downloadUri).ToLower() ?? ""; // Support for versions below 7.0
             }
 
@@ -181,10 +185,14 @@ namespace OnlineEditorsExample
             var downloadUri = (string)fileData["url"];
 
             string curExt = Path.GetExtension(fileName).ToLower();  // get current file extension
-            string downloadExt = "."+(string)fileData["filetype"];  // get the extension of the downloaded file
-
-            // TODO [Delete in version 7.0 or higher]
-            if (downloadExt == null) {
+            
+            try
+            {
+                var downloadExt = "."+(string)fileData["filetype"]; // get the extension of the downloaded file
+            }
+            catch (System.Exception)
+            {   
+                // TODO [Delete in version 7.0 or higher]
                 downloadExt = Path.GetExtension(downloadUri).ToLower(); // Support for versions below 7.0
             }
 
