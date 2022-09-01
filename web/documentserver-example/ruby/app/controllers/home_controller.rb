d2d98df5abc47193180ad68f526c8bb3555b2453
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2022-09-01 16:56:38 +0300

    support ruby v.3

--- web/documentserver-example/ruby/app/controllers/home_controller.rb
+++ web/documentserver-example/ruby/app/controllers/home_controller.rb
@@ -177,7 +177,7 @@ class HomeController < ApplicationController
       # add headers to the response to specify the page parameters
       response.headers['Content-Length'] = File.size(file_path).to_s
       response.headers['Content-Type'] = MimeMagic.by_path(file_path).eql?(nil) ? nil : MimeMagic.by_path(file_path).type
-      response.headers['Content-Disposition'] = "attachment;filename*=UTF-8\'\'" + URI.escape(file, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+      response.headers['Content-Disposition'] = "attachment;filename*=UTF-8\'\'" + ERB::Util.url_encode(file)
 
       send_file file_path, :x_sendfile => true
     rescue => ex
@@ -262,7 +262,7 @@ class HomeController < ApplicationController
     # add headers to the response to specify the page parameters
     response.headers['Content-Length'] = File.size(csvPath).to_s
     response.headers['Content-Type'] = MimeMagic.by_path(csvPath).type
-    response.headers['Content-Disposition'] = "attachment;filename*=UTF-8\'\'" + URI.escape(file_name, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+    response.headers['Content-Disposition'] = "attachment;filename*=UTF-8\'\'" + ERB::Util.url_encode(file_name)
 
     send_file csvPath, :x_sendfile => true
   end
@@ -295,7 +295,7 @@ class HomeController < ApplicationController
       # add headers to the response to specify the page parameters
       response.headers['Content-Length'] = File.size(file_path).to_s
       response.headers['Content-Type'] = MimeMagic.by_path(file_path).eql?(nil) ? nil : MimeMagic.by_path(file_path).type
-      response.headers['Content-Disposition'] = "attachment;filename*=UTF-8\'\'" + URI.escape(file_name, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+      response.headers['Content-Disposition'] = "attachment;filename*=UTF-8\'\'" + ERB::Util.url_encode(file_name)
 
       send_file file_path, :x_sendfile => true
     rescue => ex
