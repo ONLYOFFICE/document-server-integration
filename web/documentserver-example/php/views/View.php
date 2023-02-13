6f693501834d0bde82705260ce93a5fde0bb8a61
rivexe <stepan.mayorov@onlyoffice.com>
2023-02-13 16:30:00 +0300

    php: views-classes added

---
+++ web/documentserver-example/php/views/View.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * (c) Copyright Ascensio System SIA 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OnlineEditorsExamplePhp\Views;
+
+class View
+{
+    private $template;
+
+    const TEMPNAMES = [
+        "index" => "index",
+        "docEditor" => "doceditor",
+    ];
+
+    public function __construct($tempName)
+    {
+        $pathToTemplate = "./templates/".self::TEMPNAMES[$tempName].".tpl";
+        if (file_exists($pathToTemplate)) {
+            $this->template = file_get_contents($pathToTemplate);
+        } else {
+            $this->template = "";
+        }
+    }
+
+    public function parseTemplate($tagsValues = [])
+    {
+        $parsedTemplate = $this->template;
+        foreach ($tagsValues as $tag => $value) {
+            $parsedTemplate = str_replace($tag, $value, $parsedTemplate);
+        }
+        return $parsedTemplate;
+    }
+}
