067a8cce74fa02bde6202d21a7ed561915053830
vanyauhalin <ivan.uhalin@onlyoffice.com>
2023-06-02 13:03:01 +0400

    python: support for configuration via environment variables

--- web/documentserver-example/python/config.py
+++ web/documentserver-example/python/config.py
@@ -2,8 +2,15 @@ import os
 
 VERSION = '1.5.1'
 
-FILE_SIZE_MAX = 5242880
-STORAGE_PATH = 'app_data'
+if os.environ.get("FILE_SIZE_MAX"):
+    FILE_SIZE_MAX = os.environ.get("FILE_SIZE_MAX")
+else:
+    FILE_SIZE_MAX = 5242880
+
+if os.environ.get("STORAGE_PATH"):
+    STORAGE_PATH = os.environ.get("STORAGE_PATH")
+else:
+    STORAGE_PATH = 'app_data'
 
 DOC_SERV_FILLFORMS = [".docx", ".oform"]
 DOC_SERV_VIEWED = [".djvu", ".oxps", ".pdf", ".xps"]  # file extensions that can be viewed
@@ -21,18 +28,33 @@ DOC_SERV_CONVERT = [                                            # file extension
     ".xls", ".xlsb", ".xlt", ".xml"
 ]
 
-DOC_SERV_TIMEOUT = 120000
+if os.environ.get("DOC_SERV_TIMEOUT"):
+    DOC_SERV_TIMEOUT = os.environ.get("DOC_SERV_TIMEOUT")
+else:
+    DOC_SERV_TIMEOUT = 120000
 
-DOC_SERV_SITE_URL = 'http://documentserver/'
+if os.environ.get("DOC_SERV_SITE_URL"):
+    DOC_SERV_SITE_URL = os.environ.get("DOC_SERV_SITE_URL")
+else:
+    DOC_SERV_SITE_URL = 'http://documentserver/'
 
 DOC_SERV_CONVERTER_URL = 'ConvertService.ashx'
 DOC_SERV_API_URL = 'web-apps/apps/api/documents/api.js'
 DOC_SERV_PRELOADER_URL = 'web-apps/apps/api/documents/cache-scripts.html'
 DOC_SERV_COMMAND_URL='coauthoring/CommandService.ashx'
 
-EXAMPLE_DOMAIN = None
+# generates a link for example domain
+if os.environ.get("EXAMPLE_DOMAIN"):
+    EXAMPLE_DOMAIN = os.environ.get("EXAMPLE_DOMAIN")
+else:
+    EXAMPLE_DOMAIN = None
 
-DOC_SERV_JWT_SECRET = ''  # the secret key for generating token
+# the secret key for generating token
+if os.environ.get("DOC_SERV_JWT_SECRET"):
+    DOC_SERV_JWT_SECRET = os.environ.get("DOC_SERV_JWT_SECRET")
+else:
+    DOC_SERV_JWT_SECRET = ''
+
 DOC_SERV_JWT_HEADER = 'Authorization'
 DOC_SERV_JWT_USE_FOR_REQUEST = True
 
@@ -101,8 +123,3 @@ LANGUAGES = {
     'vi': 'Vietnamese',
     'aa-AA': 'Test Language'
 }
-
-if os.environ.get("EXAMPLE_DOMAIN"):  # generates a link for example domain
-    EXAMPLE_DOMAIN = os.environ.get("EXAMPLE_DOMAIN")
-if os.environ.get("DOC_SERV"):  # generates links for document server
-    DOC_SERV_SITE_URL = os.environ.get("DOC_SERV")
