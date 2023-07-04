c4c621d1f2881d823fc1510d3997ad01a5a56da6
vanyauhalin <ivan.uhalin@onlyoffice.com>
2023-07-04 17:55:36 +0400

    python-raw: fix server and proxy

--- web/documentserver-example/python/manage.py
+++ web/documentserver-example/python/manage.py
@@ -1,18 +1,14 @@
 from os import environ
-from os.path import abspath, dirname
-from pathlib import Path
 from sys import argv
 from uuid import uuid1
 from mimetypes import add_type
+from django import setup
 from django.conf import settings
 from django.core.management import execute_from_command_line
 from django.core.management.commands.runserver import Command as RunServer
-from django.core.wsgi import get_wsgi_application
 from django.urls import path
-from src.views import actions, index
 from src.history import HistoryController
-
-root = Path(dirname(abspath(__file__)))
+from src.views import actions, index
 
 def debug():
     env = environ.get('DEBUG')
@@ -31,59 +27,52 @@ def port():
     env = environ.get('PORT')
     return env or '8000'
 
-add_type('text/javascript', '.js', True)
+def configuration():
+    return {
+        'ALLOWED_HOSTS': [
+            '*'
+        ],
+        'DEBUG': debug(),
+        'ROOT_URLCONF': __name__,
+        'SECRET_KEY': uuid1(),
+        'STATIC_URL': 'static/',
+        'TEMPLATES': [
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [
+                    'templates'
+                ]
+            }
+        ]
+    }
 
-settings.configure(
-    INSTALLED_APPS=[
-        'django.contrib.staticfiles'
-    ],
-    ALLOWED_HOSTS=[
-        '*'
-    ],
-    BASE_DIR=root,
-    DEBUG=debug(),
-    ROOT_URLCONF=__name__,
-    SECRET_KEY=uuid1(),
-    STATIC_URL='static/',
-    STATICFILES_DIRS=[
-        f"{root.joinpath('static')}"
-    ],
-    TEMPLATES=[
-        {
-            'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': [
-                'templates'
-            ]
-        }
+def routers():
+    history = HistoryController()
+    return [
+        path('', index.default),
+        path('convert', actions.convert),
+        path('create', actions.createNew),
+        path('csv', actions.csv),
+        path('download', actions.download),
+        path('downloadhistory', actions.downloadhistory),
+        path('edit', actions.edit),
+        path('files', actions.files),
+        path('reference', actions.reference),
+        path('remove', actions.remove),
+        path('rename', actions.rename),
+        path('saveas', actions.saveAs),
+        path('track', actions.track),
+        path('upload', actions.upload),
+        path('history/<source_basename>', history.history),
+        path('history/<source_basename>/<version>/data', history.data)
     ]
-)
 
-history = HistoryController()
-
-urlpatterns = [
-    path('', index.default),
-    path('convert', actions.convert),
-    path('create', actions.createNew),
-    path('csv', actions.csv),
-    path('download', actions.download),
-    path('downloadhistory', actions.downloadhistory),
-    path('edit', actions.edit),
-    path('files', actions.files),
-    path('reference', actions.reference),
-    path('remove', actions.remove),
-    path('rename', actions.rename),
-    path('saveas', actions.saveAs),
-    path('track', actions.track),
-    path('upload', actions.upload),
-
-    # path('history/<source_basename>', history.history),
-    # path('history/<source_basename>/<version>/data', history.data)
-]
-
+add_type('text/javascript', '.js', True)
+settings.configure(**configuration())
+urlpatterns = routers()
 RunServer.default_addr = address()
 RunServer.default_port = port()
-
-application = get_wsgi_application()
+setup()
 
 if __name__ == '__main__':
     execute_from_command_line(argv)
