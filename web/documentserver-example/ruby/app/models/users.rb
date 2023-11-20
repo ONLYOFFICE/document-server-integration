9b6495718954e5e4d84ea62e1474a9cfda8bef4c
ZEROM22 <alexander.salyakhov@onlyoffice.com>
2023-11-20 12:39:19 +0300

    ruby: Layout/MultilineMethodParameterLineBreaks correct

--- web/documentserver-example/ruby/app/models/users.rb
+++ web/documentserver-example/ruby/app/models/users.rb
@@ -31,8 +31,18 @@ class User
                 :templates,
                 :avatar
 
-  def initialize(id, name, email, group, review_groups, comment_groups, user_info_groups, favorite,
-                 denied_permissions, descriptions, templates, avatar)
+  def initialize(id, 
+name, 
+email, 
+group, 
+review_groups, 
+comment_groups, 
+user_info_groups, 
+favorite,
+                 denied_permissions, 
+descriptions, 
+templates, 
+avatar)
     @id = id
     @name = name
     @email = email
