ced855d6aca4d00829d316b694e9d328444224eb
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-12-13 16:45:24 +0300

    delete: unused method (getConvertedData) from ServiceConverter

--- web/documentserver-example/java-spring/src/main/java/com/onlyoffice/integration/documentserver/util/service/ServiceConverter.java
+++ web/documentserver-example/java-spring/src/main/java/com/onlyoffice/integration/documentserver/util/service/ServiceConverter.java
@@ -18,16 +18,12 @@
 
 package com.onlyoffice.integration.documentserver.util.service;
 
-import com.onlyoffice.integration.dto.ConvertedData;
 import org.json.simple.JSONObject;
 import java.io.InputStream;
 
 
 // specify the converter service functions
 public interface ServiceConverter {
-    ConvertedData getConvertedData(String documentUri, String fromExtension,  // get the URL to the converted file
-                                   String toExtension, String documentRevisionId,
-                                   String filePass, Boolean isAsync, String lang);
     String generateRevisionId(String expectedKey);  // generate document key
     String convertStreamToString(InputStream stream);  // convert stream to string
     JSONObject convertStringToJSON(String jsonString);  // convert string to json
