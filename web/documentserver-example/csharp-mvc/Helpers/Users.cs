b90f333fdd1ec10f699d155ca718e942a94eec45
sshakndr <aleksandr.kondratyev@onlyoffice.com>
2023-12-29 12:47:37 +0700

    csharp-mvc: add goback Users.cs

--- web/documentserver-example/csharp-mvc/Helpers/Users.cs
+++ web/documentserver-example/csharp-mvc/Helpers/Users.cs
@@ -92,7 +92,8 @@ namespace OnlineEditorsExampleMVC.Helpers
                     new List<string>(),
                     descr_user_1,
                     true,
-                    true
+                    true,
+                    new Goback()
                 ),
             new User(
                     "uid-2",
@@ -111,7 +112,8 @@ namespace OnlineEditorsExampleMVC.Helpers
                     new List<string>(),
                     descr_user_2,
                     false,
-                    true
+                    true,
+                    new Goback("Go to Documents", null)
                 ),
             new User(
                     "uid-3",
@@ -130,7 +132,8 @@ namespace OnlineEditorsExampleMVC.Helpers
                     new List<string>() { "copy", "download", "print" },
                     descr_user_3,
                     false,
-                    false
+                    false,
+                    new Goback(null,false)
                 ),
             new User(
                     "uid-0",
@@ -144,7 +147,8 @@ namespace OnlineEditorsExampleMVC.Helpers
                     new List<string>() { "protect" },
                     descr_user_0,
                     false,
-                    false
+                    false,
+                    null
                 )
         };
 
@@ -235,7 +239,9 @@ namespace OnlineEditorsExampleMVC.Helpers
         public List<string> userInfoGroups;
         public bool avatar;
 
-        public User(string id, string name, string email, string group, List<string> reviewGroups, Dictionary<string, object> commentGroups, List<string> userInfoGroups, bool? favorite, List<string> deniedPermissions, List<string> descriptions, bool templates, bool avatar)
+        public Goback goback;
+
+        public User(string id, string name, string email, string group, List<string> reviewGroups, Dictionary<string, object> commentGroups, List<string> userInfoGroups, bool? favorite, List<string> deniedPermissions, List<string> descriptions, bool templates, bool avatar, Goback goback)
         {
             this.id = id;
             this.name = name;
@@ -249,6 +255,21 @@ namespace OnlineEditorsExampleMVC.Helpers
             this.templates = templates;
             this.userInfoGroups = userInfoGroups;
             this.avatar = avatar;
+            this.goback = goback;
+        }
+    }
+
+    public class Goback
+    {
+        public string text;
+        public bool? blank;
+
+        public Goback(){}
+
+        public Goback(string text, bool? blank)
+        {
+            this.text = text;
+            this.blank = blank;
         }
     }
 }
