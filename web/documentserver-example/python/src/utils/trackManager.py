426317587162347cc4cfc15b45876ed394cd5496
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2024-04-16 12:52:58 +0500

    refactor(python): return response in commandrequest method

--- web/documentserver-example/python/src/utils/trackManager.py
+++ web/documentserver-example/python/src/utils/trackManager.py
@@ -202,10 +202,7 @@ def commandRequest(method, key, meta=None):
     response = requests.post(config_manager.document_server_command_url().geturl(), json=payload, headers=headers,
                              verify=config_manager.ssl_verify_peer_mode_enabled(), timeout=5)
 
-    if meta:
-        return response
-
-    return
+    return response
 
 
 def resolve_process_save_body(body):
