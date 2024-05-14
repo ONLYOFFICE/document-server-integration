9a22cd66d939b52242a5bfbfd52638a9a9d71124
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2024-05-14 14:10:14 +0500

    refactor(java-spring): use sdk for fetching and deleting forgotten files

--- web/documentserver-example/java-spring/src/main/java/com/onlyoffice/integration/controllers/ForgottenController.java
+++ web/documentserver-example/java-spring/src/main/java/com/onlyoffice/integration/controllers/ForgottenController.java
@@ -18,8 +18,6 @@
 
 package com.onlyoffice.integration.controllers;
 
-import com.onlyoffice.integration.documentserver.util.file.FileUtility;
-import com.onlyoffice.integration.documentserver.managers.callback.CallbackManager;
 import com.onlyoffice.integration.dto.ForgottenFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -32,18 +30,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import com.onlyoffice.model.commandservice.CommandRequest;
+import com.onlyoffice.model.commandservice.CommandResponse;
+import com.onlyoffice.model.commandservice.commandrequest.Command;
+import com.onlyoffice.manager.document.DocumentManager;
+import com.onlyoffice.service.command.CommandService;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 @CrossOrigin("*")
 @Controller
 public class ForgottenController {
-    @Autowired
-    private FileUtility fileUtility;
-
     @Value("${server.version}")
     private String serverVersion;
 
@@ -51,7 +50,10 @@ public class ForgottenController {
     private String enableForgotten;
 
     @Autowired
-    private CallbackManager callbackManager;
+    private CommandService commandService;
+
+    @Autowired
+    private DocumentManager documentManager;
 
     @GetMapping("${url.forgotten}")
     public String index(final Model model) {
@@ -69,19 +71,21 @@ public class ForgottenController {
     private ArrayList<ForgottenFile> getForgottenFiles() {
         ArrayList<ForgottenFile> files = new ArrayList<ForgottenFile>();
         try {
-            JSONObject forgottenList = callbackManager.commandRequest("getForgottenList", null, null);
-            JSONArray keys = (JSONArray) forgottenList.get("keys");
+            CommandRequest commandRequest = CommandRequest.builder()
+                .c(Command.GET_FORGOTTEN_LIST)
+                .build();
+            CommandResponse commandResponse = commandService.processCommand(commandRequest, null);
+            List<String> keys = commandResponse.getKeys();
             for (int i = 0; i < keys.size(); i++) {
-                JSONObject result = callbackManager.commandRequest("getForgotten",
-                                                            String.valueOf(keys.get(i)),
-                                                            null);
+                commandRequest = CommandRequest.builder()
+                        .c(Command.GET_FORGOTTEN)
+                        .key(keys.get(i))
+                        .build();
+                commandResponse = commandService.processCommand(commandRequest, null);
                 ForgottenFile file = new ForgottenFile(
-                    result.get("key").toString(),
-                    fileUtility
-                        .getDocumentType(result.get("url").toString())
-                        .toString()
-                        .toLowerCase(),
-                    result.get("url").toString()
+                    commandResponse.getKey(),
+                    documentManager.getDocumentType(commandResponse.getUrl()).toString().toLowerCase(),
+                    commandResponse.getUrl()
                 );
                 files.add(file);
             }
@@ -102,7 +106,12 @@ public class ForgottenController {
         }
 
         try {
-            callbackManager.commandRequest("deleteForgotten", filename, null);
+            CommandRequest commandRequest = CommandRequest.builder()
+                .c(Command.DELETE_FORGOTTEN)
+                .key(filename)
+                .build();
+
+            CommandResponse commandResponse = commandService.processCommand(commandRequest, null);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
             e.printStackTrace();
