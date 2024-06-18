f45b6f60d45defde0a0004a95d40b021b6bb6599
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2024-06-18 13:24:51 +0500

    bind config class as singleton

--- web/documentserver-example/php-laravel/app/Providers/AppServiceProvider.php
+++ web/documentserver-example/php-laravel/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use OnlyOffice\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(Config::class, function () {
+            return new Config();
+        });
     }
 
     /**
