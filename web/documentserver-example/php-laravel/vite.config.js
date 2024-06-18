940d759e367278b3eeaed57767f4c3615c603446
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2024-06-18 13:38:27 +0500

    configure vite

--- web/documentserver-example/php-laravel/vite.config.js
+++ web/documentserver-example/php-laravel/vite.config.js
@@ -4,8 +4,19 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/media.css',
+                'resources/js/formats.js',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost'
+        },
+    },
 });
