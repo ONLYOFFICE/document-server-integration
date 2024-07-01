27e090fe3b8be6e1025f3e27afcce61464a1ee34
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2024-07-01 12:29:39 +0500

    implement document server class

---
+++ web/documentserver-example/php-laravel/onlyoffice/DocumentServer.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace OnlyOffice;
+
+use OnlyOffice\Helpers\Path;
+use Exception;
+use Illuminate\Support\Facades\Http;
+use OnlyOffice\Document\Convert\ConvertRequest;
+use OnlyOffice\Entities\File;
+use Illuminate\Support\Str;
+use OnlyOffice\Document\Command\CommandRequest;
+use OnlyOffice\Exceptions\Conversion\ConversionError;
+use OnlyOffice\Exceptions\Conversion\ConversionNotComplete;
+
+class DocumentServer
+{
+    public function __construct(
+        private Config $config,
+        private Users $users,
+        private Formats $formats,
+        private JWT $jwt,
+    ) {
+    }
+
+    public function convert(array $content): array
+    {
+        $format = $this->formats->find($content['filetype']);
+
+        if (!$format->convertible()) {
+            throw new Exception('The format is not convertible.');
+        }
+
+        $headers = [];
+
+        if ($this->config->get('jwt.enabled')) {
+            $token = $this->jwt->encode(["payload" => $content]);
+            $headers = [$this->config->get('jwt.header') => 'Bearer ' . $token];
+            $content['token'] = $this->jwt->encode($content);
+        }
+
+        $client = Http::withHeaders($headers)
+            ->timeout($this->config->get('conversion.timeout'))
+            ->asJson()
+            ->acceptJson();
+
+        if (
+            Str::of($this->config->get('conversion.url'))->isUrl(['https'])
+            && !$this->config->get('ssl_verify')
+        ) {
+            $client = $client->withoutVerifying();
+        }
+
+        $response = $client->post($this->config->get('conversion.url'), $content);
+
+        if (!$response->ok()) {
+            throw new Exception('Could not convert the file');
+        }
+
+        $result = $response->json();
+
+        if (array_key_exists('error', $result)) {
+            throw new ConversionError($result['error']);
+        }
+
+        if (!$result['endConvert']) {
+            throw new ConversionNotComplete($result['percent'], $result['filename'], $result['fileUrl']);
+        }
+
+        return $result;
+    }
+
+    public function download(string $url): string
+    {
+        $response = Http::get($url);
+
+        if (!$response->ok()) {
+            throw new Exception('Could not download the file.');
+        }
+
+        return $response->body();
+    }
+
+    public function forceSave(string $key): void
+    {
+        $request = app()->make(CommandRequest::class);
+        $request->send('forcesave', $key);
+    }
+}
