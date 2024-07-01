b7ac2f7984a6e34b65db87e4e2f9dbc6943aecab
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2024-07-01 12:40:24 +0500

    skip editor/track route csrf validation

--- web/documentserver-example/php-laravel/bootstrap/app.php
+++ web/documentserver-example/php-laravel/bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            '/editor/track',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
