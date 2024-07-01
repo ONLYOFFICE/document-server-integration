3e30d3415931f32cc40309bcf0d4ea0877065057
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2024-07-01 12:39:41 +0500

    add file disks for files and formats

--- web/documentserver-example/php-laravel/config/filesystems.php
+++ web/documentserver-example/php-laravel/config/filesystems.php
@@ -44,6 +44,21 @@ return [
             'throw' => false,
         ],
 
+        'files' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/files'),
+            'url' => env('APP_URL').'/storage/files',
+            'visibility' => 'public',
+            'throw' => true,
+        ],
+
+        'formats' => [
+            'driver' => 'local',
+            'root' => public_path('assets/document-formats'),
+            'visibility' => 'public',
+            'throw' => true,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
