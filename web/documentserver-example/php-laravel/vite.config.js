f5664fe9122dd869c940165e4482581685583b59
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2024-07-30 13:38:17 +0500

    fix incorrect path to formats js file

--- web/documentserver-example/php-laravel/vite.config.js
+++ web/documentserver-example/php-laravel/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/formats.js',
+                'resources/js/formats.js',
                 'resources/js/app.js',
                 'resources/js/forgotten.js',
             ],
