5d23ac7572a140e01785cb33f57f4c6a80328ae1
sshakndr <aleksandr.kondratyev@onlyoffice.com>
2025-03-17 13:18:00 +0700

    fix(csharp): correct download name of converted file. Fix Bug 73522

--- web/documentserver-example/csharp/Default.aspx.cs
+++ web/documentserver-example/csharp/Default.aspx.cs
@@ -469,7 +469,7 @@ namespace OnlineEditorsExample
 
                 // get the url and file type of the converted file
                 Dictionary<string, string> newFileData;
-                var result = ServiceConverter.GetConvertedData(fileUrl.ToString() , extension, conversionExtension, key, true, out newFileData, filePass, lang);
+                var result = ServiceConverter.GetConvertedData(fileUrl.ToString() , extension, conversionExtension, key, true, out newFileData, filePass, lang, _fileName);
                 if (result != 100)
                 {
                     return "{ \"step\" : \"" + result + "\", \"filename\" : \"" + _fileName + "\"}";
