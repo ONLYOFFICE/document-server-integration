721f2fbf6cfa71371bff0e2caa459f40933d1148
sshakndr <aleksandr.kondratyev@onlyoffice.com>
2025-06-17 16:40:37 +0700

    fix(csharp-mvc): change uploaded file name if converted

--- web/documentserver-example/csharp-mvc/Scripts/jscript.js
+++ web/documentserver-example/csharp-mvc/Scripts/jscript.js
@@ -182,6 +182,7 @@ if (typeof jQuery != "undefined") {
                     if (response.step && response.step < 100) {
                         checkConvert(filePass, fileType);
                     } else {
+                        jq("#uploadFileName").text(response.filename);
                         jq("#step2").addClass("done").removeClass("current");
                         loadScripts();
                     }
