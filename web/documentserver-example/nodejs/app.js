688ee867f5259df65e779eea28b15741c28dc76d
sshakndr <aleksandr.kondratyev@onlyoffice.com>
2026-04-02 16:12:26 +0700

    feat(nodejs): disable AI plugin for anonymous
    # Conflicts:
    #	CHANGELOG.md

--- web/documentserver-example/nodejs/app.js
+++ web/documentserver-example/nodejs/app.js
@@ -1250,6 +1250,10 @@ app.get('/editor', (req, res) => { // define a handler for editing document
       pluginsConfig = plugins;
     }
 
+    if (userid === 'uid-0') {
+      pluginsConfig = { ...pluginsConfig, disable: ['asc.{9DC93CDB-B576-4F0C-B55E-FCC9C48DD007}'] };
+    }
+
     // file config data
     const argss = {
       apiUrl: siteUrl + configServer.get('apiUrl'),
